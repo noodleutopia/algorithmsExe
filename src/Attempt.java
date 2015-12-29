780e2733b57470eb71d91ee7e7ee6d9ba8b9602d
noodleutopia <noodleutopia@outlook.com>
2015-12-29 17:20:47 +0800

    done 232

--- src/Attempt.java
+++ src/Attempt.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 import binary_tree.TreeNode;
 import linked_list.ListNode;
@@ -462,4 +463,41 @@ public class Attempt {
         }
         return res;
     }
+    /**
+   	 * PROBLEM 232
+   	 */
+    class MyQueue {
+    	public Stack<Integer> stack1 = new Stack<Integer>();
+    	public Stack<Integer> stack2 = new Stack<Integer>();
+
+        // Push element x to the back of queue.
+        public void push(int x) {
+            stack1.push(x);
+        }
+
+        // Removes the element from in front of queue.
+        public void pop() {
+        	while(stack1.size() > 1) {
+                stack2.push(stack1.pop());
+        	}
+        	stack1.pop();
+        	while(!stack2.isEmpty()) {
+        		stack1.push(stack2.pop());
+        	}
+        }
+
+        // Get the front element.
+        public int peek() {
+        	while(stack1.size() > 1) {
+                stack2.push(stack1.pop());
+        	}
+        	return stack1.peek();
+        }
+
+        // Return whether the queue is empty.
+        public boolean empty() {
+            return stack1.isEmpty();
+        }
+    }
+    
 }
